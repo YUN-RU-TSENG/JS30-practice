86253b3a63bd1bbccde07de1d9de2a0a0d404043
YUN-RU-TSENG <coco002197@gmail.com>
2020-05-18 17:51:45 +0800

    [doc] add function explain 增加對於沒有使用 Array.from 的註解

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,5 +1,6 @@
 (() => {
   var inputs = document.querySelectorAll(".controls input");
+  // 不使用 Array.from 或是解構是因為 Nodelist 本身可以使用 forEach
   inputs.forEach((element) => element.addEventListener("input", changeValue));
 
   function changeValue(e) {
