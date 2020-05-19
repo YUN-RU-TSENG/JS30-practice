55e7c107bbfd93ec7f34564d6dbadb81eb9094ae
YUN-RU-TSENG <coco002197@gmail.com>
2020-05-20 00:37:41 +0800

    [refactor] function of js

--- 05 - Flex Panel Gallery/main.js
+++ 05 - Flex Panel Gallery/main.js
@@ -1,6 +1,7 @@
 (()=>{
 
-  var panels = document.querySelectorAll('div.panel')
+  var panels;
+  panels = document.querySelectorAll('div.panel')
 
   panels.forEach((panel) => panel.addEventListener('click', addClass))
   panels.forEach((panel) => panel.addEventListener('transitionend', addClassSecond))
@@ -10,10 +11,8 @@
    * 添加、刪除第一種樣式，展開寬度
    */
   function addClass(e){
-    panel.classList.toggle('panel-change')
-    panels.forEach(el => { // 這裡的 this 是為了除了自己的都會移除樣式，使用到了 this！
-      el !== this && el.classList.remove('panel-change')
-    })
+    e.target.classList.toggle('panel-change')
+    panels.forEach(el => el !== e.target && el.classList.remove('panel-change')) // 這裡的 this 是為了除了自己的都會移除樣式，使用到了 this！但是我後來覺得使用 e.target 更符合邏輯
   }
 
   /**
@@ -21,7 +20,7 @@
    * 添加、刪除第二種延遲視覺樣式，移動字體
    */
   function addClassSecond(e){
-    panel.classList.toggle('panel-changes')
+    e.target.classList.toggle('panel-changes')
   }
 
 })();
