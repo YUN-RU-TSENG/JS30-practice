cc3881742efa221c1cad1f666596c4b274ef2d34
YUN-RU-TSENG <coco002197@gmail.com>
2020-05-20 15:38:30 +0800

    [refactor] css

--- 05 - Flex Panel Gallery/main.js
+++ 05 - Flex Panel Gallery/main.js
@@ -3,14 +3,14 @@
   var panels;
   panels = document.querySelectorAll('.panel')
 
-  panels.forEach((panel) => panel.addEventListener('click', addClass))
+  panels.forEach((panel) => panel.addEventListener('click', addClassAndClearOthersClass))
   panels.forEach((panel) => panel.addEventListener('transitionend', addClassSecond))
 
   /**
    *
    * 添加、刪除第一種樣式，展開寬度
    */
-  function addClass(e){
+  function addClassAndClearOthersClass(e){
     e.currentTarget.classList.toggle('panel-change')
     // 刪除除了自己的樣式
     panels.forEach(el => el !== e.currentTarget && el.classList.remove('panel-change')) // 這裡的 this 是為了除了自己的都會移除樣式，使用到了 this！但是我後來覺得使用 e.target 更符合這裡一致性
