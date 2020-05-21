db3e686b7aa51ae3755aad9a097e3a0dd1b2a1d1
YUN-RU-TSENG <coco002197@gmail.com>
2020-05-21 17:42:33 +0800

    [refactor] reload fun

--- 06 - Type Ahead/main.js
+++ 06 - Type Ahead/main.js
@@ -1,15 +1,15 @@
 (() => {
-  var baseURL =
+  const baseURL =
     "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
-  var cities = [];
+  const cities = [];
   fetch(baseURL)
     .then((response) => response.json())
-    .then((data) => cities.push(...data));
+    .then((data) => cities.push(...data))
+    .catch((err) => console.error(err))
 
-  var searchInput = document.querySelector(".search");
-  var suggestions = document.querySelector(".suggestions");
-  console.log("cities", cities);
+  const searchInput = document.querySelector(".search");
+  const suggestions = document.querySelector(".suggestions");
 
   searchInput.addEventListener("input", displayKeyWord);
 
@@ -32,10 +32,12 @@
    */
   function displayKeyWord(e) {
     const keyWord = e.currentTarget.value;
+    !keyWord && location.reload(); // 每次不刪選時重新家載到最初頁面
+
     const regexp = new RegExp(keyWord, 'gi'); // 會每次編譯，使用/\/不會每次編譯
     const filterCities = filterWord(cities, regexp);
 
-    const displayCity = filterCities.map((place) => {
+    suggestions.innerHTML = filterCities.map((place) => {
       const cityName = place.city.replace(regexp, `<span class="hl">${keyWord}</span>`)
       const stateName = place.state.replace(regexp, `<span class="hl">${keyWord}</span>`)
 
@@ -46,8 +48,6 @@
       </li>`
       //  <span class="population">${numeral(city.population).format(0,000)}</span>
     }).join('');
-
-    suggestions.innerHTML = displayCity;
   }
 
 })();
