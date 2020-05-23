f02bdb655d284600a7407091e84bc7a5d0e63ab3
YUN-RU-TSENG <coco002197@gmail.com>
2020-05-23 21:13:39 +0800

    [style] formating

--- 08 - Fun with HTML5 Canvas/main.js
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -1,53 +1,61 @@
 (() => {
-  const canvas = document.querySelector('canvas');
-  const canvasContext = canvas.getContext('2d');
-  canvas.width = canvas.parentElement.clientWidth;
-  canvas.height = 700;
-  canvasContext.strokeStyle = '#19caad';
-  canvasContext.lineCap = 'square';
-  canvasContext.lineJoin = 'minter';
-  canvasContext.lineWidth = 100
+  const canvas = document.querySelector("canvas");
+  const canvasContext = canvas.getContext("2d");
 
+  setCanvas(canvas.parentElement.clientWidth, 700, "square", 0);
+
+  canvas.addEventListener("mousemove", drawInCanvas);
+  canvas.addEventListener("mousedown", openDraw);
+  canvas.addEventListener("mouseup", closeDraw);
+
+  let [huePath, colorPath, isActive] = [true, true, false];
   let hue = 360;
-  let lastX = 0;
-  let lastY = 0;
-  let isActive = false;
-  let colorPath = true;
-  let huePath = true;
+  let [lastX, lastY] = [null, null];
+  const [MAX_HUE_OF_HSL, MIN_HUE_OF_HSL] = [360, 1];
 
-  canvas.addEventListener('mousemove', function(e){
+  /**
+   * 設置基礎 canvas 細節
+   *
+   * @param {*} width
+   * @param {*} height
+   * @param {*} lineCap
+   * @param {*} lineWidth
+   */
+  function setCanvas(width, height, lineCap, lineWidth) {
+    [canvas.width, canvas.height] = [width, height];
+    [canvasContext.lineCap, canvasContext.lineWidth] = [lineCap, lineWidth];
+  }
 
-    if(!isActive) return
+  /**
+   * 繪製畫布
+   *
+   * @param {*} e
+   */
+  function drawInCanvas(e) {
+    // 非正在點擊的滑鼠移動不算
+    if (!isActive) return;
 
-    canvasContext.strokeStyle = `hsl(${hue}, 90%, 80%)`
-    canvasContext.beginPath()
-
+    // 設定筆畫顏色
+    canvasContext.strokeStyle = `hsl(${hue}, 90%, 80%)`;
+    // 設定開始的新筆畫
+    canvasContext.beginPath();
+    // 移動到上次路徑
     canvasContext.moveTo(lastX, lastY);
-
+    // 繪製上次路徑到這次路徑的線條
     canvasContext.lineTo(e.offsetX, e.offsetY);
+    // 描邊該路徑
     canvasContext.stroke();
+
+    // 設定筆畫全域變數變色，下一次會重新設定一次筆畫顏色
+    if (hue === MAX_HUE_OF_HSL || hue === MIN_HUE_OF_HSL) huePath = !huePath;
+    // 當顏色循環環，從另一個方向重新循環
+    huePath ? hue ++ : hue --;
+
+    // 設定筆畫寬度全域變數變化
+    if (canvasContext.lineWidth === 10 || canvasContext.lineWidth === 1) colorPath = !colorPath;
+    colorPath ? canvasContext.lineWidth ++ : canvasContext.lineWidth --;
+    // 設定本次的最終路徑儲存
     [lastX, lastY] = [e.offsetX, e.offsetY];
-
-    if (hue >= 360 || hue <= 1) {
-      huePath = !huePath
-    }
-
-    console.log(huePath)
-    if(huePath){
-      hue += 1;
-    }else{
-      hue -= 1;
-    }
-
-    if(canvasContext.lineWidth >= 100 || canvasContext.lineWidth <= 1){
-      colorPath = !colorPath
-    }
-
-    if(colorPath){
-      canvasContext.lineWidth ++
-    }else{
-      canvasContext.lineWidth --
-    }
 
     // console.log('e.clientX', e.clientX)
     // console.log('e.offsetX', e.offsetX)
@@ -55,14 +63,15 @@
     // console.log('e.screenX', e.screenX)
     // console.log('e.layerX', e.layerX)
 
-  })
+  }
 
-  canvas.addEventListener('mousedown', (e)=>{
+  function openDraw(e) {
     isActive = !isActive;
     [lastX, lastY] = [e.offsetX, e.offsetY];
-  })
-  canvas.addEventListener('mouseup', ()=>{
-    isActive = !isActive
-  })
+  }
+
+  function closeDraw(e) {
+    isActive = !isActive;
+  }
 
 })();
