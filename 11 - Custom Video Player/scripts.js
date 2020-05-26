7945cda5d24c4be18955c35d000a500024b5b66a
YUN-RU-TSENG <coco002197@gmail.com>
2020-05-26 16:17:10 +0800

    [style] commit change

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,4 +1,4 @@
-void function playVideoIIFE() {
+void (function playVideoIIFE() {
 
   const video = document.querySelector(".player .viewer");
   const toggle = document.querySelector(".player .toggle");
@@ -9,19 +9,27 @@ void function playVideoIIFE() {
 
   /**
    * play pause change
+   *
+   * @param {*} event
+   * @param {*} videoElement
    */
+  function toggleVideo(event, videoElement) {
+    videoElement.paused ? videoElement.play() : videoElement.pause();
+  }
   toggle.addEventListener("click", function (event) {
     void toggleVideo.call(this, event, video);
   });
 
-  function toggleVideo(event, videoElement) {
-    videoElement.paused ? videoElement.play() : videoElement.pause();
-  }
-
-  
   /**
    * play icon change
+   *
+   * @param {*} event
+   * @param {*} toggleElement
    */
+  function switchVideoIcon(event, toggleElement) {
+    const icon = event.currentTarget.paused ? "►" : "❚❚";
+    toggleElement.textContent = icon;
+  }
   video.addEventListener("pause", function (event) {
     void switchVideoIcon.call(this, event, toggle);
   });
@@ -29,65 +37,64 @@ void function playVideoIIFE() {
     void switchVideoIcon.call(this, event, toggle);
   });
 
-  function switchVideoIcon(event, toggleElement) {
-    const icon = event.currentTarget.paused ? "►" : "❚❚";
-    toggleElement.textContent = icon;
-  }
-
   /**
-   * play progressbar display change
+   * updateTimeDisplay
+   *
+   * @param {*} event
+   * @param {*} progressElement
    */
-  video.addEventListener("timeupdate", function (event) {
-    void updateTimeDisplay.call(this, event, progressBar);
-  });
-
   function updateTimeDisplay(event, progressElement) {
     const percent =
       (event.currentTarget.currentTime / event.currentTarget.duration) * 100;
     progressElement.style.flexBasis = `${percent}%`;
   }
-
+  video.addEventListener("timeupdate", function (event) {
+    void updateTimeDisplay.call(this, event, progressBar);
+  });
 
   /**
    * skip video time
+   *
+   * @param {*} event
+   * @param {*} videoElement
    */
+  function skipVideo(event, videoElement) {
+    videoElement.currentTime += parseFloat(event.currentTarget.dataset.skip);
+  }
   skipButtons.forEach((skipButton) => {
     skipButton.addEventListener("click", function (event) {
       void skipVideo.call(this, event, video);
     });
   });
 
-  function skipVideo(event, videoElement) {
-    videoElement.currentTime += parseFloat(event.currentTarget.dataset.skip);
-  }
-
-
   /**
    * update volume and speed time
+   *
+   * @param {*} event
+   * @param {*} videoElement
    */
+  function updateValue(event, videoElement) {
+    videoElement[event.currentTarget.name] = event.currentTarget.value;
+  }
   ranges.forEach((range) => {
     range.addEventListener("input", function (event) {
       void updateValue.call(this, event, video);
     });
   });
 
-  function updateValue(event, videoElement) {
-    videoElement[event.currentTarget.name] = event.currentTarget.value;
-  }
-
-
   /**
    * scroll progress to change vide time
+   *
+   * @param {*} event
+   * @param {*} videoElement
    */
-  progress.addEventListener("click", function (event) {
-    return updateProgressBar.call(this, event, video);
-  });
-
   function updateProgressBar(event, videoElement) {
     const percent =
       (event.offsetX / event.currentTarget.offsetWidth) *
       parseFloat(videoElement.duration);
     videoElement.currentTime = percent;
   }
-
-}();
+  progress.addEventListener("click", function (event) {
+    return updateProgressBar.call(this, event, video);
+  });
+})();
