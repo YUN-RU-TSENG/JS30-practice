e76bfd5523905c09d0520a05fe463a65e0a7961e
YUN-RU-TSENG <coco002197@gmail.com>
2020-05-27 15:13:14 +0800

    [style] fix by ESLint

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,106 +1,104 @@
-void function playVideoIIFE() {
-
-  const video = document.querySelector(".player .viewer");
-  const toggle = document.querySelector(".player .toggle");
-  const ranges = document.querySelectorAll(".player .player__slider");
-  const progress = document.querySelector(".player .progress");
-  const skipButtons = document.querySelectorAll(".player [data-skip]");
-  const progressBar = document.querySelector(".player .progress__filled");
+(function playVideoIIFE() {
+  const video = document.querySelector('.player .viewer')
+  const toggle = document.querySelector('.player .toggle')
+  const ranges = document.querySelectorAll('.player .player__slider')
+  const progress = document.querySelector('.player .progress')
+  const skipButtons = document.querySelectorAll('.player [data-skip]')
+  const progressBar = document.querySelector('.player .progress__filled')
 
   /**
    * play pause change
    *
-   * @param {*} event
-   * @param {*} videoElement
+   * @param {Object} event
+   * @param {Object} videoElement
    */
   function toggleVideo(event, videoElement) {
-    videoElement.paused ? videoElement.play() : videoElement.pause();
+    videoElement.paused ? videoElement.play() : videoElement.pause()
   }
-  toggle.addEventListener("click", function (event) {
-    void toggleVideo.call(this, event, video);
-  });
+  toggle.addEventListener('click', function callback(event) {
+    void toggleVideo.call(this, event, video)
+  })
 
   /**
    * play icon change
    *
-   * @param {*} event
-   * @param {*} toggleElement
+   * @param {Object} event
+   * @param {Object} toggleElement
    */
   function switchVideoIcon(event, toggleElement) {
-    const pauseIcon = '<span>&#10074;&#10074;<span>';
+    const pauseIcon = '<span>&#10074;&#10074;<span>'
     const playIcon = '<span>&#9658;<span>'
 
     toggleElement.innerHTML = event.currentTarget.paused ? playIcon : pauseIcon
   }
-  video.addEventListener("pause", function (event) {
-    void switchVideoIcon.call(this, event, toggle);
-  });
-  video.addEventListener("play", function (event) {
-    void switchVideoIcon.call(this, event, toggle);
-  });
+  video.addEventListener('pause', function callback(event) {
+    void switchVideoIcon.call(this, event, toggle)
+  })
+  video.addEventListener('play', function callback(event) {
+    void switchVideoIcon.call(this, event, toggle)
+  })
 
   /**
    * updateTimeDisplay
    *
-   * @param {*} event
-   * @param {*} progressElement
+   * @param {Object} event
+   * @param {Object} progressElement
    */
   function updateTimeDisplay(event, progressElement) {
-    const videoCurrentTime = event.currentTarget.currentTime;
-    const videoTime = event.currentTarget.duration;
+    const videoCurrentTime = event.currentTarget.currentTime
+    const videoTime = event.currentTarget.duration
 
-    progressElement.style.flexBasis = `${(videoCurrentTime / videoTime) * 100}%`;
+    progressElement.style.flexBasis = `${(videoCurrentTime / videoTime) * 100}%`
   }
-  video.addEventListener("timeupdate", function (event) {
-    void updateTimeDisplay.call(this, event, progressBar);
-  });
+  video.addEventListener('timeupdate', function callback(event) {
+    void updateTimeDisplay.call(this, event, progressBar)
+  })
 
   /**
    * skip video time
    *
-   * @param {*} event
-   * @param {*} videoElement
+   * @param {Object} event
+   * @param {Object} videoElement
    */
   function skipVideo(event, videoElement) {
-    videoElement.currentTime += parseFloat(event.currentTarget.dataset.skip);
+    videoElement.currentTime += parseFloat(event.currentTarget.dataset.skip)
     // videoElement.currentTime += +(event.currentTarget.dataset.skip);
     // videoElement.currentTime += Number(event.currentTarget.dataset.skip);
   }
   skipButtons.forEach((skipButton) => {
-    skipButton.addEventListener("click", function (event) {
-      void skipVideo.call(this, event, video);
-    });
-  });
+    skipButton.addEventListener('click', function callback(event) {
+      void skipVideo.call(this, event, video)
+    })
+  })
 
   /**
    * update volume and speed time
    *
-   * @param {*} event
-   * @param {*} videoElement
+   * @param {Object} event
+   * @param {Object} videoElement
    */
   function updateValue(event, videoElement) {
-    videoElement[event.currentTarget.name] = event.currentTarget.value;
+    videoElement[event.currentTarget.name] = event.currentTarget.value
   }
   ranges.forEach((range) => {
-    range.addEventListener("input", function (event) {
-      void updateValue.call(this, event, video);
-    });
-  });
+    range.addEventListener('input', function callback(event) {
+      void updateValue.call(this, event, video)
+    })
+  })
 
   /**
    * scroll progress to change vide time
    *
-   * @param {*} event
-   * @param {*} videoElement
+   * @param {Object} event
+   * @param {Object} videoElement
    */
   function updateProgressBar(event, videoElement) {
-    const [curseOfElementDistance, elementWidth] = [event.offsetX, event.currentTarget.offsetWidth];
-    const videoTime = videoElement.duration;
+    const [curseOfElementDistance, elementWidth] = [event.offsetX, event.currentTarget.offsetWidth]
+    const videoTime = videoElement.duration
 
-    videoElement.currentTime = (curseOfElementDistance  / elementWidth) * videoTime
+    videoElement.currentTime = (curseOfElementDistance / elementWidth) * videoTime
   }
-  progress.addEventListener("click", function (event) {
-    void updateProgressBar.call(this, event, video);
-  });
-
-}();
+  progress.addEventListener('click', function callback(event) {
+    void updateProgressBar.call(this, event, video)
+  })
+}())
