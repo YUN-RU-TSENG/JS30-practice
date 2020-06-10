97a021ab015543b2930667e56829f097522463d4
YUN-RU-TSENG <coco002197@gmail.com>
2020-06-10 20:51:58 +0800

    [style] format code

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,7 +1,7 @@
 (() => {
 
-  var inputs = document.querySelectorAll(".controls input");
-  var selector = document.querySelector(".controls select")
+  var inputs = document.querySelectorAll(".controls input"),
+      selector = document.querySelector(".controls select");
 
   // 不使用 Array.from 或是解構是因為 Nodelist 本身可以使用 forEach
   inputs.forEach((element) => element.addEventListener("input", changeValue));
